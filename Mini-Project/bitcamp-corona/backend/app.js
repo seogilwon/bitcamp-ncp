4a5ce66ff664bcd9f5d59257a1a6b628f9ad73e7
seogilwon <sgw111@naver.com>
2022-12-22 17:59:16 +0900

    221222_study

--- Mini-Project/bitcamp-corona/backend/app.js
+++ Mini-Project/bitcamp-corona/backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const request = require("request");
-const port = 3000;
 const app = express();
 
 app.get('/proxy', (req, res) => {
@@ -10,12 +9,11 @@ app.get('/proxy', (req, res) => {
 
 
     request.get({
-      url: "http://apis.data.go.kr/1790387/covid19CurrentStatusKorea/covid19CurrentStatusKoreaJason?" +
-      "serviceKey=s2eMCzdkGrxPMhVr%2Bhv2U3bF%2Bj3ui%2B7qUS4r0tJKms0%2Bm%2FDN2TCj76L0o%2BacXVPei8K2lxKVFICQraz0iZgJ5g%3D%3D&"
+      url: "http://apis.data.go.kr/1790387/covid19CurrentStatusKorea/covid19CurrentStatusKoreaJason?serviceKey=s2eMCzdkGrxPMhVr%2Bhv2U3bF%2Bj3ui%2B7qUS4r0tJKms0%2Bm%2FDN2TCj76L0o%2BacXVPei8K2lxKVFICQraz0iZgJ5g%3D%3D&"
     }, (error, response, body) => {
       // res.send(JSON.stringify(body));
       res.send(body);
 
   });
 });
-app.listen(3000, () => console.log(`${port}번 포트에서 서버 시작했음!`));
+app.listen(3000, () => console.log('서버 시작'));
