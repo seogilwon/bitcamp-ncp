dc0257e05adfb694b9fe5f36d1db8134272ff569
seogilwon <sgw111@naver.com>
2022-12-30 17:57:01 +0900

    221230-study

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,70 +1,104 @@
 package bitcamp.myapp;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 public class App {
+
+  // static 으로 선언한 변수는 static 멤버끼리 공유할 수 있다.
+  static final int SIZE = 100;
+  static int count = 0;
+
+  static int[] no = new int[SIZE];
+  static String[] name = new String[SIZE];
+  static String[] tel = new String[SIZE];
+  static String[] postNo = new String[SIZE];
+  static String[] basicAddress = new String[SIZE];
+  static String[] detailAddress = new String[SIZE];
+  static boolean[] working = new boolean[SIZE];
+  static char[] gender = new char[SIZE];
+  static byte[] level = new byte[SIZE];
+  static String[] createdDate = new String[SIZE];
+
   public static void main(String[] args) {
 
-    // 키보드에서 입력을 받는 도구 준비
+    // 코드를 메서드로 분리했으면 호출하라!
+    inputMembers();
+
+    System.out.println();
+
+    // 코드를 메서드로 분리했으면 호출하라!
+    printMembers();
+
+  } // main()
+
+  static void inputMembers() {
     Scanner keyScanner = new Scanner(System.in);
 
-    System.out.print("번호? ");
-    int no = Integer.parseInt(keyScanner.nextLine());  //대문자로시작=class=도구함// 소문자로시작=값
+    for (int i = 0; i < SIZE; i++) {
 
-    System.out.print("이름? ");
-    String name = keyScanner.nextLine();
+      no[i] = promptInt(keyScanner, "번호? ");
+      name[i] = promptString(keyScanner, "이름? ");
+      tel[i] = promptString(keyScanner, "전화? ");
+      postNo[i] = promptString(keyScanner, "우편번호? ");
+      basicAddress[i] = promptString(keyScanner, "주소1? ");
+      detailAddress[i] = promptString(keyScanner, "주소2? ");
+      working[i] = promptInt(keyScanner, "0. 미취업\n1. 재직중\n재직자? ") == 1;
+      gender[i] = promptInt(keyScanner, "0. 남자\n1. 여자\n성별? ") == 0 ? 'M' : 'W';
+      level[i] = (byte) promptInt(keyScanner, "0. 비전공자\n1. 준전공자\n2. 전공자\n전공? ");
+      Date today = new Date(System.currentTimeMillis());
+      createdDate[i] = today.toString();
 
-    System.out.print("전화? ");
-    String tel = keyScanner.nextLine();
+      count++;
 
-    System.out.print("우편번호? ");
-    String postNo = keyScanner.nextLine();
-
-    System.out.print("주소1? ");
-    String basicAddress = keyScanner.nextLine();
-
-    System.out.print("주소2? ");
-    String detailAddress = keyScanner.nextLine();
-
-    System.out.print("재직자?(true/false) ");
-    boolean working = Boolean.parseBoolean(keyScanner.nextLine());
-
-    System.out.print("성별?(남자: M, 여자:W) ");
-    char gender = keyScanner.nextLine().charAt(0);  //M(남자), W(여자)
-
-    System.out.println("0. 비전공자");
-    System.out.println("1. 준전공자");
-    System.out.println("2. 전공자");
-    System.out.print("전공? ");
-    byte level = Byte.parseByte(keyScanner.nextLine());  //0(비전공자), 1(준전공자), 2(전공자)
-
-    System.out.print("가입일?(예: 2022-12-25) ");
-    String createdDate = keyScanner.nextLine();
-
-
-
-
-    //System.out.println(no);
-    //system도구함안. 작업수행에필요한'값'. 작업을수행(작업할때사용할'값')
-    //=도구함=class.   =변수=field.         =작업자=연산자=메소드=펑션(파라미터)
-
-    System.out.printf("번호: %d\n", no);  //%d = 변수의 값(no) 삽입 // \n = escape character:문자가아니라명령이다.
-    System.out.printf("이름: %s\n", name);
-    System.out.printf("전화: %s\n", tel);
-    System.out.printf("우편번호: %s\n", postNo);
-    System.out.printf("주소1: %s\n", basicAddress);
-    System.out.printf("주소2: %s\n", detailAddress);
-    System.out.printf("재직여부: %s\n", working ? "예" : "아니오");
-    System.out.printf("성별: %s\n", gender == 'M' ? "남자" : "여자");
-
-    String levelTitle;
-    switch(level) {
-      case 0 : levelTitle = "비전공자"; break;
-      case 1 : levelTitle = "준정공자"; break;
-      default : levelTitle = "전공자";
+      String str = promptString(keyScanner, "계속 입력하시겠습니까?(Y/n) ");
+      if (!str.equalsIgnoreCase("Y") && str.length() != 0) {
+        break;
+      }
     }
-    System.out.printf("전공: %s\n", levelTitle);
-    System.out.printf("가입일: %s\n", createdDate);
 
+    keyScanner.close();
   }
-}
+
+  static void printMembers() {
+    for (int i = 0; i < count; i++) {
+      System.out.printf("번호: %d\n", no[i]);
+      System.out.printf("이름: %s\n", name[i]);
+      System.out.printf("전화: %s\n", tel[i]);
+      System.out.printf("우편번호: %s\n", postNo[i]);
+      System.out.printf("주소1: %s\n", basicAddress[i]);
+      System.out.printf("주소2: %s\n", detailAddress[i]);
+      System.out.printf("재직자: %s\n", working[i] ? "예" : "아니오");
+      System.out.printf("성별: %s\n", gender[i] == 'M' ? "남자" : "여자");
+
+      String levelTitle;
+      switch (level[i]) {
+        case 0: levelTitle = "비전공자"; break;
+        case 1: levelTitle = "준전공자"; break;
+        default: levelTitle = "전공자";
+      }
+      System.out.printf("전공: %s\n", levelTitle);
+
+      System.out.printf("가입일: %s\n", createdDate[i]);
+
+      System.out.println("---------------------------------------");
+    }
+  }
+
+  static String promptString(Scanner scanner, String title) {
+    System.out.print(title);
+    return scanner.nextLine();
+  }
+
+  static int promptInt(Scanner scanner, String title) {
+    return Integer.parseInt(promptString(scanner, title));
+  }
+
+} // class App
+
+
+
+
+
+
+
