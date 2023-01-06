e9c049971207fa3ed7973d075ff2530a27f6dc5b
seogilwon <sgw111@naver.com>
2023-01-06 16:58:52 +0900

    230106-study

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -4,21 +4,35 @@ public class App {
 
   public static void main(String[] args) {
     goMainMenu();
+    System.out.println("안녕히 가세요!");
 
-    System.out.println("bye");
-
+    // 프로그램이 사용한 자원 해제하기
     Prompt.close();
-  }
+  } // main()
 
   private static void goMainMenu() {
+    // 일반회원 목록을 저장할 메모리를 준비한다
+    MemberHandler generalMemberHandler = new MemberHandler("일반학생");
+
+    // 국비지원 학생 목록을 저장할 메모리를 준비한다.
+    MemberHandler supportedMemberHandler = new MemberHandler("국비지원학생");
+
+    // 기업 위탁 교육생목록을 저장할 메모리를 준비한다
+    MemberHandler companyMemberHandler = new MemberHandler("위탁교육생");
+
     while (true) {
-      System.out.println("1. 회원관리");
+      System.out.println("1. 일반학생관리");
+      System.out.println("2. 국비지원학생관리");
+      System.out.println("3. 위탁교육생관리");
       System.out.println("9. 종료");
       int menuNo = Prompt.inputInt("메뉴> ");
 
       if (menuNo == 1) {
-        MemberHandler.service();
-
+        generalMemberHandler.service();
+      } else if (menuNo == 2) {
+        supportedMemberHandler.service();
+      } else if (menuNo == 3) {
+        companyMemberHandler.service();
       } else if (menuNo == 9) {
         break;
       } else {
@@ -26,4 +40,13 @@ public class App {
       }
     }
   }
-}
+} // class App
+
+
+
+
+
+
+
+
+
