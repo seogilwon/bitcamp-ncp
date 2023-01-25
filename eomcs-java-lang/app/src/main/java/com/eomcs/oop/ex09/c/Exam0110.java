1d0f19cb1898f6d43cc28986b886e6f6b31145c2
seogilwon <sgw111@naver.com>
2023-01-25 17:58:16 +0900

    230125-study

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex09/c/Exam0110.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex09/c/Exam0110.java
@@ -44,27 +44,27 @@ public class Exam0110 {
     System.out.println("------------------------------------");
 
     // 2) 인터페이스의 레퍼런스 사용
-    // - 인터페이스에 정의된 메서드 호출 가능 
+    // - 인터페이스에 정의된 메서드 호출 가능
     // - 상위 인터페이스에 정의된 메서드 호출 가능
-    ProtocolB b = obj;
-    b.rule2(); // OK --> ProtocolB.rule2() 
+    ProtocolB b = obj;                            //'ProtocolB 레퍼런스 타입' 기준으로 찾는다.
+    b.rule2(); // OK --> ProtocolB.rule2()
     b.rule1(); // OK --> ProtocolA.rule1()
-    //    b.m1(); // 컴파일 오류 --> ProtocolImpl.m1() 
+    //    b.m1(); // 컴파일 오류 --> ProtocolImpl.m1()
 
     System.out.println("-------------------------------");
 
     // 3) 수퍼 인터페이스의 레퍼런스 사용
-    // - 인터페이스에 정의된 메서드 호출 가능 
+    // - 인터페이스에 정의된 메서드 호출 가능
     // - 상위 인터페이스에 정의된 메서드 호출 가능
     // - ProtocolImpl 클래스가 ProtocolB 의 규칙에 따라 제작되었다면
     //   결국 그 수퍼 인터페이스의 규칙도 준수하는 것이 된다.
     // - 따라서 수퍼 인터페이스 레퍼런스에 객체를 담을 수 있다.
     //   즉 수퍼 인터페이스 레퍼런스로 객체를 가리킬 수 있다.
     //
-    ProtocolA a = obj;
+    ProtocolA a = obj;                            //'ProtocolA 레퍼런스 타입' 기준으로 찾는다.
     a.rule1(); // OK --> ProtocolA.rule1()
-    //    a.rule2(); // 컴파일 오류 --> ProtocolB.rule2() 
-    //    a.m1(); // 컴파일 오류 --> ProtocolImpl.m1() 
+    //    a.rule2(); // 컴파일 오류 --> ProtocolB.rule2()
+    //    a.m1(); // 컴파일 오류 --> ProtocolImpl.m1()
   }
 
   public static void main(String[] args) {
--- myapp/app/src/main/java/bitcamp/util/LinkedList.java
+++ myapp/app/src/main/java/bitcamp/util/LinkedList.java
@@ -109,6 +109,7 @@ public class LinkedList implements List {
     return -1;
   }
 
+  @Override
   public int size() {
     return this.size;
   }
@@ -127,6 +128,13 @@ public class LinkedList implements List {
       i++;
     }
     return cursor.value;
+  }
+
+  @Override
+  public Iterator iterator() {
+    // 이 LinkedList 객체에서 데이터를 꺼내주는 일을 할
+    // Iterator 구현체를 만들어 리턴한다.
+    return new ListIterator(this);
   }
 }
 
