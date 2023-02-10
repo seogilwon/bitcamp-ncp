92c8712cdf7075ec1ee26f17fceef2dc3747894e
seogilwon <sgw111@naver.com>
2023-02-10 18:01:24 +0900

    230210-study

--- myapp/app-server/src/main/java/bitcamp/myapp/dao/JdbcStudentDao.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/dao/JdbcStudentDao.java
@@ -1,7 +1,6 @@
 package bitcamp.myapp.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -9,11 +8,16 @@ import bitcamp.myapp.vo.Student;
 
 public class JdbcStudentDao implements StudentDao {
 
+  Connection con;
+
+  // 의존객체 Connection 을 생성자에서 받는다.
+  public JdbcStudentDao(Connection con) {
+    this.con = con;
+  }
+
   @Override
   public void insert(Student s) {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
-        Statement stmt = con.createStatement()) {
+    try (Statement stmt = con.createStatement()) {
 
       String sql = String.format(
           "insert into app_student(name, tel, pst_no, bas_addr, det_addr, work, gender, level)"
@@ -36,9 +40,7 @@ public class JdbcStudentDao implements StudentDao {
 
   @Override
   public Student[] findAll() {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
-        Statement stmt = con.createStatement();
+    try (Statement stmt = con.createStatement();
         ResultSet rs = stmt.executeQuery(
             "select student_id, name, tel, work, level"
                 + " from app_student"
@@ -68,9 +70,7 @@ public class JdbcStudentDao implements StudentDao {
 
   @Override
   public Student findByNo(int no) {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
-        Statement stmt = con.createStatement();
+    try (Statement stmt = con.createStatement();
         ResultSet rs = stmt.executeQuery(
             "select student_id, name, tel, created_date, pst_no, bas_addr, det_addr, work, gender, level"
                 + " from app_student"
@@ -99,10 +99,42 @@ public class JdbcStudentDao implements StudentDao {
   }
 
   @Override
+  public Student[] findByKeyword(String keyword) {
+    try (Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(
+            "select student_id, name, tel, work, level"
+                + " from app_student"
+                + " where name like('%" + keyword + "%')"
+                + " or tel like('%" + keyword + "%')"
+                + " or bas_addr like('%" + keyword + "%')"
+                + " or det_addr like('%" + keyword + "%')"
+                + " order by student_id desc")) {
+
+      ArrayList<Student> list = new ArrayList<>();
+      while (rs.next()) {
+        Student s = new Student();
+        s.setNo(rs.getInt("student_id"));
+        s.setName(rs.getString("name"));
+        s.setTel(rs.getString("tel"));
+        s.setWorking(rs.getBoolean("work"));
+        s.setLevel(rs.getByte("level"));
+
+        list.add(s);
+      }
+
+      Student[] arr = new Student[list.size()];
+      list.toArray(arr);
+
+      return arr;
+
+    } catch (Exception e) {
+      throw new DaoException(e);
+    }
+  }
+
+  @Override
   public void update(Student s) {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
-        Statement stmt = con.createStatement()) {
+    try (Statement stmt = con.createStatement()) {
 
       String sql = String.format(
           "update app_student set "
@@ -128,9 +160,7 @@ public class JdbcStudentDao implements StudentDao {
 
   @Override
   public boolean delete(Student s) {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
-        Statement stmt = con.createStatement()) {
+    try (Statement stmt = con.createStatement()) {
 
       String sql = String.format("delete from app_student where student_id=%d", s.getNo());
 
