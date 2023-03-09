4316166c449c0546d03e555ad1376c02772b3a82
seogilwon <sgw111@naver.com>
2023-03-09 18:09:38 +0900

    230309-study

--- myapp/app-server/src/main/java/bitcamp/myapp/vo/Board.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/vo/Board.java
@@ -14,12 +14,12 @@ public class Board implements java.io.Serializable {
   private String content;
   private String password;
 
-  // Jackson 라이브러리가 Date 타입 값을 문자열로 변환할 때 사용할 규칙을 설정한다.
+  // Jackson 라이브러리가 Date 타입 값을 JSON 문자열로 변환할 때 사용할 규칙을 설정한다.
   @JsonFormat(
       shape = Shape.STRING,
-      pattern = "yyyy-MM-dd"
-      )
+      pattern = "yyyy-MM-dd")
   private Date createdDate;
+
   private int viewCount;
   private int writerNo;
   private String writerName;
